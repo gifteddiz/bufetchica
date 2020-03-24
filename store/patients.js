7b502962b8f9a929eb1c737ea371e3e4999f9314
Andrey Medvedev <gifteddiz@gmail.com>
2020-03-24 10:16:06 +0500

    Исправлена сортировка в списке палат

--- store/patients.js
+++ store/patients.js
@@ -127,7 +127,9 @@ export const getters = {
       )
         result.push(element.ward);
     });
-    return result.sort();
+    return result.sort((a, b) => {
+      return parseInt(a - b);
+    });
   },
   getFilteredPatients: state => filter => {
     var patients = [];
